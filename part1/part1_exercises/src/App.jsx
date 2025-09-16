631720853dea87e05fbeed161f706d910ac23ae1
Sebastian Iilomo <sebastian.iilomo@edu.turkuamk.fi>
2025-09-16 13:55:31 +0300

    Committing exercise 1.2 (Updated App.jsx)
    exe 1.2

--- part1/part1_exercises/src/App.jsx
+++ part1/part1_exercises/src/App.jsx
@@ -5,10 +5,26 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   console.log(props)
   return (
     <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Content = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
+    </div>
   )
 }
 
@@ -22,20 +38,15 @@ const Total = (props) => {
 
 const App = () => {
   const header = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
   const total = 'Number of exercises in total'
   const totalnumber = exercises1 + exercises2 + exercises3
   return (
     <div>
       <Header header={header}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content />
       <Total total={total} totalnumber={totalnumber}/>
     </div>
   )
